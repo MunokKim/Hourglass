a28a71ab4528616d28d795c8db9ce6f4d838c053
Munok <wer0222@naver.com>
2018-08-31 21:50:16 +0900

    MainVC, NewWorkVC, WorkingVC 생성 및 뷰 형태 제작, 컨트롤러 연결

---
+++ Hourglass/MainViewController.swift
@@ -0,0 +1,117 @@
+//
+//  MainViewController.swift
+//  Hourglass
+//
+//  Created by 김문옥 on 2018. 8. 31..
+//  Copyright © 2018년 김문옥. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    @IBAction func addWorkBtn(_ sender: Any) {
+        
+        if let view = self.storyboard?.instantiateViewController(withIdentifier: "NewWorkViewController") as? UIViewController {
+            self.present(view, animated: true, completion: nil)
+        }
+    }
+    
+    @IBOutlet var mainTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // 네비게이션 컨트롤러에서 large title 켜기
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell", for: indexPath) as UITableViewCell
+
+        // Configure the cell...
+        cell.textLabel?.text = "hi"
+
+        return cell
+    }
+ 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // go to workingVC
+        if let view = self.storyboard?.instantiateViewController(withIdentifier: "WorkingViewController") {
+            self.navigationController?.pushViewController(view, animated: true)
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hourglass/WorkingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WorkingViewController.swift
+//  Hourglass
+//
+//  Created by 김문옥 on 2018. 8. 31..
+//  Copyright © 2018년 김문옥. All rights reserved.
+//
+
+import UIKit
+
+class WorkingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // 네비게이션 컨트롤러 하위의 뷰에서는 large title 비활성화 하기
+        navigationItem.largeTitleDisplayMode = .never
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
